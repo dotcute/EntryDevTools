6d48f6acc443b9febd98d0861fb8f47ddd772726
cham_why <53082717+sjufkaus2@users.noreply.github.com>
2020-03-18 20:46:16 +0900

    Access to browser

--- devtools/devtools.js
+++ devtools/devtools.js
@@ -1,15 +1,25 @@
+const getBrowser = () => {
+  const browserType = navigator.userAgent.toLowerCase();
+  if(browserType.indexOf("chrome") != -1){
+    return chrome;
+  }else if(browserType.indexOf("firefox") != -1){
+    return browser;
+  }
+}
+const browsers = getBrowser();
+
 const handleShown = () => {
   console.log("panel is being shown");
 }
 
 const handleHidden = () => {
-  console.log("panel is being hidden");  
+  console.log("panel is being hidden");
 }
 
-browser.devtools.panels.create(
+browsers.devtools.panels.create(
   "엔트리",
-  "/icons/logo.png",
-  "/devtools/panel/panel.html"
+  "./icons/logo.png",
+  "./devtools/panel/panel.html"
 ).then((newPanel) => {
   newPanel.onShown.addListener(handleShown);
   newPanel.onHidden.addListener(handleHidden);
