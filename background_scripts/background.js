1506d504517b211837a4be42f652b43273c15be3
thoratica <thoratica.2019@gmail.com>
2020-04-03 13:34:09 +0900

    Update background.js

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -1,32 +1,25 @@
-
 const getBrowser = () => {
   const browserType = navigator.userAgent.toLowerCase();
-  if(browserType.indexOf("chrome") != -1){
+  if(browserType.indexOf('chrome') != -1){
     return chrome;
-  }else if(browserType.indexOf("firefox") != -1){
+  }else if(browserType.indexOf('firefox') != -1){
     return browser;
   }
-}
+};
 const browsers = getBrowser();
 
-const handleMessage = async (req, sender, sendResponse) => {
-
-  // if (sender.url != browsers.runtime.getURL("/devtools/panel/panel.html")) {
-  //   return;
-  // }
-
-  if (req.action == "readObject") {
-    await browsers.tabs.executeScript(null,{file:"../ObjectManager/ObjectSender.js"});
+const handleMessage = async (req) => {
+  if (req.action == 'readObject') {
+    await browsers.tabs.executeScript(null, {file:'../ObjectManager/ObjectSender.js'});
     await browsers.tabs.query({active: true, currentWindow: true}, async (tabs) => {
-      await browsers.tabs.sendMessage(tabs[0].id, {action: "readObject"}, (response) => {
-          console.log(response);
-          browsers.runtime.sendMessage({action: "sendObjectContainer",source:response});
+      await browsers.tabs.sendMessage(tabs[0].id, {action: 'readObject'}, (response) => {
+          browsers.runtime.sendMessage({action: 'sendObjectContainer', source:response});
       });
     });
   }
 
-  if(req.script) browsers.tabs.executeScript(req.tabId,{code: req.script});
+  if(req.script) browsers.tabs.executeScript(req.tabId, {code: req.script});
 
-}
+};
 
 browsers.runtime.onMessage.addListener(handleMessage);
